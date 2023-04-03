f10158b4fc7447d51edcf1b81c78dfcbc805b9ca
Patrick <raffertyp@msoe.edu>
2023-04-03 15:02:21 -0500

    Fix format issues with Sequence Reader

--- PLYSequenceTesting/Assets/C# Scripts/PLYSequenceReader.cs
+++ PLYSequenceTesting/Assets/C# Scripts/PLYSequenceReader.cs
@@ -1,112 +1,99 @@
 using System;
-using System.IO;
-using UnityEngine;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using UnityEngine;
 
-public class PLYSequenceReader : MonoBehaviour
-{
+public class PLYSequenceReader : MonoBehaviour {
     public string plyFile;
+    private Mesh _mesh;
 
-    private StreamReader reader;
-    private Mesh mesh;
+    private StreamReader _reader;
 
-    void Start()
-    {
-        mesh = new Mesh();
-        mesh.MarkDynamic();
-        GetComponent<MeshFilter>().mesh = mesh;
+    private void Start() {
+        _mesh = new Mesh();
+        _mesh.MarkDynamic();
+        GetComponent<MeshFilter>().mesh = _mesh;
         // Check if the ply File is null before attempting to open it.
-        if (plyFile != null)
-        {
-            reader = new StreamReader(plyFile);
-            string line = reader.ReadLine();
-            if (!line.Equals("plys"))
-            {
+        if (plyFile != null) {
+            _reader = new StreamReader(plyFile);
+            string line = _reader.ReadLine();
+            if (!line.Equals("plys")) {
                 throw new FormatException("File did not have plys header");
             }
-            
+
             List<string> sequenceHeader = new List<string>();
-            
-            while (!line.Equals("end_sequence_header"))
-            {
-                line = reader.ReadLine();
+
+            while (!line.Equals("end_sequence_header")) {
+                line = _reader.ReadLine();
                 sequenceHeader.Add(line);
             }
 
-            InvokeRepeating("renderFrame", 0f,
-                1 / float.Parse(sequenceHeader.Find(s => s.StartsWith("framerate ")).Split(' ')[1])); // This invoke repeating method runs once per frame time amount of time.
+            InvokeRepeating(nameof(RenderFrame), 0f,
+                1 / float.Parse(sequenceHeader.Find(s => s.StartsWith("framerate "))
+                    .Split(' ')[
+                        1])); // This invoke repeating method runs once per frame time amount of time.
         }
-        else
-        {
+        else {
             Debug.LogError("PLY file is not assigned");
         }
+    }
+
+    private void OnDestroy() {
+        _reader.Close();
     }
 
     /*
     This method gets vertex and color information from the ".plys" file and 
     assigns the data to the verticies and colors List objects.
     */
-    private void renderFrame()
-    {
-        string line = reader.ReadLine();
-        if (!line.Equals("ply"))
-        {
+    private void RenderFrame() {
+        string line = _reader.ReadLine();
+        if (!line.Equals("ply")) {
             throw new FormatException("Header not found.");
         }
 
         List<string> header = new List<string>();
-        while (!line.Equals("end_header"))
-        {
-            line = reader.ReadLine();
+        while (!line.Equals("end_header")) {
+            line = _reader.ReadLine();
             header.Add(line);
         }
 
-        int vertexCount = int.Parse(header.Find(s => s.StartsWith("element vertex")).Split(" ")[2]);
+        int vertexCount = int.Parse(header.Find(s => s.StartsWith("element vertex ")).Split(" ")[2]);
 
         List<Vector3> vertices = new List<Vector3>(vertexCount);
         List<Color> colors = new List<Color>(vertexCount);
 
-        switch (header.Find(s => s.StartsWith("format")))
-        {
+        switch (header.Find(s => s.StartsWith("format "))) {
             case "format ascii 1.0":
                 List<string> lines = new List<string>(vertexCount);
-                for (int i = 0; i < vertexCount; i++)
-                {
-                    lines.Add(reader.ReadLine());
+                for (int i = 0; i < vertexCount; i++) {
+                    lines.Add(_reader.ReadLine());
                 }
 
-                foreach (string vertex in lines.AsParallel())
-                {
+                foreach (string vertex in lines.AsParallel()) {
                     string[] lineContents = vertex.Split(" ");
-
-                    /*
-                Get the xyz components of the vertice and add it to the List of vertices.
-                */
+                    // Get the xyz components of the vertex and add it to the List of vertices.
                     float x = float.Parse(lineContents[0]);
                     float y = float.Parse(lineContents[1]);
                     float z = float.Parse(lineContents[2]);
-
-                    /*
-                    Now we grab the color from the file.
-                    */
+                    // Now we grab the color from the file.
                     float r = float.Parse(lineContents[3]);
                     float g = float.Parse(lineContents[4]);
                     float b = float.Parse(lineContents[5]);
                     // Divide each color component by 255 to allow the Color constructor to parse correctly.
-
                     vertices.Add(new Vector3(x, y,
                         z)); //Add the new Vector3 that describes the point in space to the vector Array.
                     colors.Add(new Color(r / 255f, g / 255f, b / 255f));
                 }
 
-                reader.ReadLine();
+                _reader.ReadLine();
                 break;
             case "format binary_little_endian 1.0":
-                throw new Exception("TODO"); //todo
+                throw new NotImplementedException();
             case "format binary_big_endian 1.0":
-                throw new Exception("TODO"); //todo
+                throw new NotImplementedException();
             default:
                 throw new FormatException("Could not find file format.");
         }
@@ -114,15 +101,9 @@ public class PLYSequenceReader : MonoBehaviour
         int[] indices = new int[vertexCount];
         Parallel.For(0, vertexCount, i => indices[i] = i);
 
-        mesh.Clear();
-        mesh.vertices = vertices.ToArray();
-        mesh.colors = colors.ToArray();
-        mesh.SetIndices(indices, MeshTopology.Points, 0);
-        
-    }
-
-    private void OnDestroy()
-    {
-        reader.Close();
+        _mesh.Clear();
+        _mesh.vertices = vertices.ToArray();
+        _mesh.colors = colors.ToArray();
+        _mesh.SetIndices(indices, MeshTopology.Points, 0);
     }
 }
